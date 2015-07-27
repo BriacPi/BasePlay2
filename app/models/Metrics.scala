32a92ba2f2e53689c594d128a6193c062ae6a64e
Briac-Piriou <briac.piriou@orange.fr>
2015-07-27 11:48:30 +0200

    for thomas

---
+++ app/models/Metrics.scala
@@ -0,0 +1,31 @@
+package models
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsPath, Reads, _}
+
+
+case class Metrics(metrics: Map[String, Metric]) {
+  def getCodesToNamesMap:Map[String,String]={
+    metrics.mapValues(_.label)
+  }
+}
+case class Metric(label:String,formatString:String, growth:String, invertGrowth:Boolean, description:String, formatGrowth:String)
+
+
+
+object Metric {
+  implicit val metricReader: Reads[Metric] = (
+    (JsPath \ "label").read[String] and
+      (JsPath \ "format_string").read[String] and
+      (JsPath \ "growth").read[String] and
+      (JsPath \ "invert_growth").read[Boolean] and
+      (JsPath \ "description").read[String] and
+      (JsPath \ "format_growth").read[String]
+    )(Metric.apply _)
+}
+
+
+
+object Metrics {
+  implicit val metricsReader: Reads[Metrics] = Json.reads[Metrics]
+}
--- test/library/SuspectRowSpec.scala
+++ test/library/SuspectRowSpec.scala
@@ -15,7 +15,7 @@ class SuspectRowSpec extends Specification{
   "doctor-strange" should {
 
     "get the test data in the test Database" in  new WithApplication{
-      SuspectRow.all().head mustEqual SuspectRow(2,java.time.LocalDate.parse("2000-01-01"),"caisse","groupe","agence","pdv",
+      SuspectRow.all().head mustEqual new SuspectRow(2,java.time.LocalDate.parse("2000-01-01"),"caisse","groupe","agence","pdv",
         "metric",Status.Solved,Nature.Abnormality, java.time.LocalDate.parse("2000-01-01"), "admin","comment")
     }
 
