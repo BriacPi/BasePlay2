600f08b8d275e4dd602f71fe768d144ed44cbc34
Briac-Piriou <briac.piriou@orange.fr>
2015-07-30 11:49:39 +0200

    try:color

--- public/javascripts/stateUpdater.js
+++ public/javascripts/stateUpdater.js
@@ -1,5 +1,10 @@
 $(function(){
     setInterval(function(){
-        $.getJSON("/analysis_state").then(function(data){$( '#state' ).html(data.niceMessage);})
-    }, 10000);
+        $.getJSON("/analysis_state").then(function(data){
+            if(false) {$( '#state' ).html( "<a >"+ data.niceMessage + "</a>");
+            }
+            else {$( '#state' ).html( "<a class=errorRed>"+ data.niceMessage + "</a>");
+            }
+                                                        })
+                             }, 10000);
 })
